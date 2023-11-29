e7e6703510a056fa6650b040a392e61315810181
dunno <dunnotoru@yandex.ru>
2023-11-30 00:51:28 +0700

    TicketSaleView

--- UI/ViewModel/PassengerViewModel.cs
+++ UI/ViewModel/PassengerViewModel.cs
@@ -3,12 +3,54 @@
     public class PassengerViewModel : ViewModelBase
     {
         private string _name;
+        private string _surname;
+        private string _patronymic;
+        private string _series;
+        private string _number;
+
         public string Name
         {
-            get => "amongus";
+            get => _name;
             set
             {
+                _name = value;
                 NotifyPropertyChanged(nameof(Name));
+            }
+        }
+        public string Surname
+        {
+            get => _surname;
+            set
+            {
+                _surname = value;
+                NotifyPropertyChanged(nameof(Surname));
+            }
+        }
+        public string Patronymic
+        {
+            get => _patronymic;
+            set
+            {
+                _patronymic = value;
+                NotifyPropertyChanged(nameof(Patronymic));
+            }
+        }
+        public string Series
+        {
+            get => _series;
+            set
+            {
+                _series = value;
+                NotifyPropertyChanged(nameof(Series));
+            }
+        }
+        public string Number
+        {
+            get => _number;
+            set
+            {
+                _number = value;
+                NotifyPropertyChanged(nameof(Number));
             }
         }
     }
--- UI/ViewModel/TicketSaleViewModel.cs
+++ UI/ViewModel/TicketSaleViewModel.cs
@@ -12,9 +12,19 @@ namespace UI.ViewModel
         private DateTime _departureDateTime;
         private DateTime _arrivalDateTime;
         private int _price;
+        private PassengerViewModel _selectedPassenger;
         
         public ObservableCollection<PassengerViewModel> Passengers {  get; set; }
-        public PassengerViewModel SelectedPassenger {  get; set; }
+        
+        public PassengerViewModel SelectedPassenger
+        {
+            get => _selectedPassenger;
+            set
+            {
+                _selectedPassenger = value;
+                NotifyPropertyChanged(nameof(SelectedPassenger));
+            }
+        }
 
         public string DepartureStation
         {
@@ -67,14 +77,30 @@ namespace UI.ViewModel
         public ICommand DeletePassengerCommand { get; }
 
 
-
         public NavigateCommand DeclineCommand { get; }
         public NavigateCommand SellCommand { get; }
 
         public TicketSaleViewModel()
         {
-            Passengers = new ObservableCollection<PassengerViewModel>();
-            Passengers.Add(new PassengerViewModel());
+            Passengers = new ObservableCollection<PassengerViewModel>()
+            {
+                new PassengerViewModel()
+                {
+                    Name = "имечко",
+                    Surname = "фамилия",
+                    Patronymic = "отчествович",
+                    Series = "123",
+                    Number = "123243"
+                },
+                new PassengerViewModel()
+                {
+                    Name = "четкий",
+                    Surname = "потсан",
+                    Patronymic = "крижевич",
+                    Series = "1233",
+                    Number = "1231"
+                },
+            };
         }
     }
 }
