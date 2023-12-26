9a7179b6103516fd4781fa006f5f6f3971bd834c
dunno <dunnotoru@yandex.ru>
2023-12-27 02:00:52 +0700

    Валидация остальных

--- UI/View/Dispatcher/VehicleMenuUserControl.xaml.cs
+++ UI/View/Dispatcher/VehicleMenuUserControl.xaml.cs
@@ -8,5 +8,17 @@ namespace UI.View
         {
             InitializeComponent();
         }
+
+        private void NumberPreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            foreach (char c in e.Text)
+            {
+                if (!char.IsDigit(c))
+                {
+                    e.Handled = true;
+                    break;
+                }
+            }
+        }
     }
 }
